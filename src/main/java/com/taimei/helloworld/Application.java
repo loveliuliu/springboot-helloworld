222e1a274bdfeba9257f60aa38c7a2eab9fee0f4
pengxuanchen <andy.chen@guanaitong.com>
2018-12-12 16:53:30 +0800

    modify

--- src/main/java/com/taimei/helloworld/Application.java
+++ src/main/java/com/taimei/helloworld/Application.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+        System.out.println("Welcome to Taimei.");
     }
-
-
 }
